def336d5927d7b6e4531303c8c6d8fafaac84665
legnakbs <rkdqudtn623@naver.com>
2026-04-10 16:24:43 +0900

    UI -1

--- Source/Soulslike/Soulslike.Build.cs
+++ Source/Soulslike/Soulslike.Build.cs
@@ -14,6 +14,7 @@ public class Soulslike : ModuleRules
 			"Engine",
 			"InputCore",
 			"EnhancedInput",
+			"GameplayAbilities",
 			"AIModule",
 			"StateTreeModule",
 			"GameplayStateTreeModule",
